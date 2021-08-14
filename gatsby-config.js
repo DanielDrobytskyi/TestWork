693cd0497b2b8f67daf0e5ae5424015aa8e3d3e2
Daniel <67470687+DanielDrobytskyi@users.noreply.github.com>
2021-08-14 19:05:42 +0300

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
